5ef8cb219a091987737e5a5f40449650783db7f0
Sergei Bykov <ss.bykoff@gmail.com>
2024-06-30 06:00:54 +0300

    добавлен метод onDraw

--- app/src/main/java/ru/netology/statsview/ui/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -1,6 +1,8 @@
 package ru.netology.statsview.ui
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
 import android.view.View
@@ -20,9 +22,22 @@ class StatsView @JvmOverloads constructor(
 ) {
     private var radius = 0F
     private var center = PointF()
+    private val lineWidth = AndroidUtils.dp(context, 5)
+    private val paint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        strokeWidth = lineWidth.toFloat()
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        radius = min(w, h) / 2F - AndroidUtils.dp(context, 5)
+        radius = min(w, h) / 2F - lineWidth
         center = PointF(w / 2F, h / 2F)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawCircle(center.x, center.y, radius, paint)
     }
 }
