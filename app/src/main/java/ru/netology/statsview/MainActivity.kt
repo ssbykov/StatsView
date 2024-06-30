3a382b94c821a552b33845e056bf41617ec7f4d6
Sergei Bykov <ss.bykoff@gmail.com>
2024-06-30 06:51:35 +0300

    добавлена отрисовка текста

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        findViewById<StatsView>(R.id.statsView).data = listOf(
+            0.30F,
+            0.30F,
+            0.40F,
+            0.10F,
+        )
     }
 }
--- app/src/main/java/ru/netology/statsview/ui/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import ru.netology.statsview.utils.AndroidUtils
 import kotlin.math.min
+import kotlin.random.Random
 
 class StatsView @JvmOverloads constructor(
     context: Context,
@@ -20,8 +22,14 @@ class StatsView @JvmOverloads constructor(
     defStyleAttr,
     defStyleRes
 ) {
+    var data = emptyList<Float>()
+        set(value) {
+            field = value
+            invalidate()
+        }
     private var radius = 0F
     private var center = PointF()
+    private var oval = RectF()
     private val lineWidth = AndroidUtils.dp(context, 5)
     private val paint = Paint(
         Paint.ANTI_ALIAS_FLAG
@@ -31,13 +39,40 @@ class StatsView @JvmOverloads constructor(
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
     }
+    private val textPaint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        textSize = AndroidUtils.dp(context, 20).toFloat()
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         radius = min(w, h) / 2F - lineWidth
         center = PointF(w / 2F, h / 2F)
+        oval = RectF(
+            center.x - radius,
+            center.y - radius,
+            center.x + radius,
+            center.y + radius,
+        )
     }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawCircle(center.x, center.y, radius, paint)
+        if (data.isNotEmpty()) {
+            var startAngle = -90F
+            data.forEach {
+                val angle = it * 360
+                paint.color = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+                canvas.drawArc(oval, startAngle, angle, false, paint)
+                startAngle += angle
+            }
+            canvas.drawText(
+                "%.2f%%".format(data.sum() * 100),
+                center.x,
+                center.y + textPaint.textSize / 4,
+                textPaint
+            )
+        }
     }
 }
